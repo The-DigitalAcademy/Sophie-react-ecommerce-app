61f8d1187e2af7548a38e3ca5b7966e011422184
Sophie <sophie.sa43@gmail.com>
2022-07-20 16:07:37 +0200

    updated it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-
-import './App.css';
+import { ThemeProvider } from '@mui/system';
+import { Button, Container } from '@mui/material';
+import { useEffect } from 'react';
+import theme from './styles/theme';
+import Appbar from './components/appbar';
 
 function App() {
+
+  useEffect(() => {
+    document.title = "React Material UI - Home";
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Welcome to Ecommerce</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="xl" sx={{ background: '#fff' }}>
+
+        <Appbar />
+        {
+          /*
+          Banner
+          Promotions
+          title
+          Products
+          footer
+          searchbox
+          appdrawer
+          */
+        }
+        <Button variant='contained'>Test</Button>
+      </Container>
+    </ThemeProvider>
   );
 }
 
